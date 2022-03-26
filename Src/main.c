6a51e39977e6b98d7efeaf18d089d7d97837573d
Hao Lingjie <15065057+lingjiehao@users.noreply.github.com>
2022-03-26 14:37:49 +0800

    init rtt:SEGGER_RTT_Init()

--- Src/main.c
+++ Src/main.c
@@ -100,9 +100,7 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   long i = 0;
-  SEGGER_RTT_ConfigUpBuffer(0, NULL, NULL, 0, SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
-
-  SEGGER_RTT_ConfigUpBuffer(0, NULL, NULL, 0, SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
+  SEGGER_RTT_Init();
 
   SEGGER_RTT_WriteString(0, "SEGGER Real-Time-Terminal Sample\r\n\r\n");
   SEGGER_RTT_WriteString(0, "###### Testing SEGGER_printf() ######\r\n");
